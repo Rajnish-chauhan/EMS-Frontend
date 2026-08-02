3e290c579101299d090021bff76164c29aa5428b
Rajnish Chauhan <crajnish425@gmail.com>
2026-08-03 01:55:03 +0530

    update url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'https://ems-backend-vqa1.onrender.com/'; 
+const API_URL = 'https://ems-backend-vqa1.onrender.com/api/v1/ems'; 
 export const getEmployees = () => axios.get(API_URL);
 export const createEmployee = (employee) => axios.post(API_URL, employee);
 export const deleteEmployee = (id) => axios.delete(`${API_URL}/${id}`);
